1c871f166333f11a4d27aac00a51483c683e4e0a
hurm989 <hurmat.naz223@gmail.com>
2022-05-13 17:29:31 +0500

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import todolist from './todolist';
 
 function App() {
+  const [text, setText] = useState("")
+  const [items, setItems] = useState([])
+  let listOfItems = () => {
+    setItems((olditems) => {
+      return [...olditems, text]
+    })
+    setText('')
+  }
+  let deleteItems = (ind) => {
+    const updatedItem = items.filter((elem, id) => {
+      return ind != id;
+    })
+    setItems(updatedItem)
+  }
+  let remv = () => {
+    setItems([])
+  }
+  let updte = (ind) => {
+    let editedTaskList = items.map((ele, indx) => {
+      if (indx == ind) {
+        items[indx] = prompt()
+
+      }
+    })
+    setItems([...items])
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>To Do List</h3>
+        <input value={text} onChange={(e) => { setText(e.target.value) }} type="text" placeholder='Add an item' />
+        <button onClick={listOfItems}>Add</button>
+        <ol>
+
+          {items.map((itemval, ind) => {
+            return (<div key={ind}> <button onClick={() => deleteItems(ind)}>Delete</button>
+              <li>{itemval} <button onClick={() => updte(ind)}>Edit</button></li></div>)
+          }
+          )}
+        </ol>
+        <button onClick={remv}>Remove All</button>
       </header>
     </div>
   );
---
+++ src/todolist.js
@@ -0,0 +1,5 @@
+import React from "react";
+const todolist =(props)=>{
+    return <li>{props.textOne}</li>
+}
+export default todolist;
